da78dc1d73ae03fe3020da4486df29389eb6fe01
Wanderson Alves <alves.wanderson@outlook.com>
2021-09-28 21:02:50 -0300

    Payment, one-to-one association

--- src/main/java/com/wanderson/course/config/TestConfig.java
+++ src/main/java/com/wanderson/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wanderson.course.entities.Category;
 import com.wanderson.course.entities.Order;
 import com.wanderson.course.entities.OrderItem;
+import com.wanderson.course.entities.Payment;
 import com.wanderson.course.entities.Product;
 import com.wanderson.course.entities.User;
 import com.wanderson.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
